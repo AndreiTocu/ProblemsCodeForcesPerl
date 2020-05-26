eeddd4dfc3be29ed4f0de770554492b8fd6d2f48
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-26 14:10:13 +0300

    Problem 757A

---
+++ Problem 757A_debbuged.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use List::Util qw(min);
+
+my %letters = ('B' => 0, 'b' => 0, 'u' => 0, 'a' => 0, 'r' => 0, 'l' => 0, 's' => 0);
+
+sub countLetters {
+  my($str) = @_;
+  
+  for my $chr (split //, $str) {
+    if(exists $letters{$chr}) {
+      $letters{$chr} = $letters{$chr} + 1;
+    }
+  }
+}
+
+sub countWrdBul {
+  
+  my %ref= @_;
+  
+  my $answer = $ref{'B'};
+  $answer = min($answer, $ref{'u'} / 2);
+  $answer = min($answer, $ref{'a'} / 2);
+  $answer = min($answer, $ref{'b'});
+  $answer = min($answer, $ref{'l'});
+  $answer = min($answer, $ref{'r'});
+  $answer = min($answer, $ref{'s'});
+
+  print $answer;
+}
+
+my $str = <>;
+
+countLetters($str);
+
+countWrdBul(%letters);
